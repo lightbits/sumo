2054203006b9878f7e7d0f12f866857d2312e989
lightbits <simenh.jobb@gmail.com>
2015-09-13 11:09:41 +0200

    Debug draw RT has per-component masking

--- shaders/debug-draw-rt.fs
+++ shaders/debug-draw-rt.fs
@@ -2,10 +2,17 @@
 
 in vec2 v_texel;
 uniform sampler2D channel;
-uniform vec4 mask;
+uniform vec4 maskr;
+uniform vec4 maskg;
+uniform vec4 maskb;
+uniform vec4 maska;
 out vec4 f_color;
 
 void main()
 {
-    f_color = texture(channel, v_texel) * mask;
+    vec4 sample = texture(channel, v_texel);
+    f_color = sample.r * maskr +
+              sample.g * maskg +
+              sample.b * maskb +
+              sample.a * maska;
 }
