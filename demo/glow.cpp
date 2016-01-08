1fb90c6d2c44a2b04862d316d5e91cb2fa04d121
lightbits <simenh.jobb@gmail.com>
2016-01-08 17:37:26 +0100

    Better parameters for glow effect

--- demo/glow.cpp
+++ demo/glow.cpp
@@ -43,7 +43,8 @@ float OrenNayar(vec3 lightDirection,
     float NdotE = max(dot(viewDirection, surfaceNormal), 0.0f);
 
     //float s = roughness*roughness;
-    float s = roughness; // Whatever
+    // Let's instead just assume roughness = sigma^2
+    float s = roughness;
     float A = 1.0 - 0.5 * s / (s + 0.33);
     float B = 0.45 * s / (s + 0.09);
     float k1 = max(0.0, LdotE-NdotE*NdotL);
@@ -100,7 +101,7 @@ in vec2 v_position;
 uniform sampler2D channel0;
 uniform float time;
 out vec4 f_color;
-const int SAMPLES = 129;
+const int SAMPLES = 24;
 float random(vec2 co)
 {
     co += vec2(0.53*time, 0.701*time);
@@ -112,22 +113,22 @@ void main()
 
     vec2 glowCenter = vec2(0.0, 0.0);
     vec2 p = v_position;
-    vec2 dp = 0.7 * (v_position - glowCenter) / float(SAMPLES);
+    vec2 dp = 0.5 * (v_position - glowCenter) / float(SAMPLES);
     float glow = 0.0;
     float weight = 1.0;
     for (int i = 0; i < SAMPLES; i++)
     {
         float data = texture(channel0, vec2(0.5) + 0.5 * p).a;
-        glow += data * weight * mix(0.8, 1.0, random(uv));
-        weight *= 0.95;
-        p -= dp * mix(0.9, 1.0, random(uv));
+        glow += data * weight * mix(0.95, 1.0, random(uv));
+        weight *= 0.92;
+        p -= dp * mix(0.8, 1.0, random(uv));
     }
 
     vec4 pass = texture(channel0, uv);
     f_color.rgb = pass.rgb + vec3(pass.a);
     f_color.rgb += 0.1 * glow * vec3(1.3, 1.2, 1.0);
 
-    f_color.rgb += 0.08*vec3(1.0-0.3*uv.y, 0.0, 0.5+0.5*uv.x);
+    f_color.rgb += 0.1*vec3(1.0-0.3*uv.y, 0.0, 0.5+0.5*uv.x);
     f_color.a = 1.0;
 }
 );
